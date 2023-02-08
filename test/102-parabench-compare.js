96c87e514d23770fa9af556d2b27a56e0d406c0f
Konstantin S. Uvarin <khedin@gmail.com>
2023-02-08 01:58:55 +0100

    [api] Add squashData static method to post-process comparison results

--- test/102-parabench-compare.js
+++ test/102-parabench-compare.js
@@ -7,16 +7,27 @@ describe( 'BigoBench.compare', () => {
   it( 'produces some results', done => {
     const bench = new ParaBench();
 
-    bench.compare({ argList: [13, 21, 34, 55]}, {
+    bench.compare({ maxTime: 0.05 }, {
       fwd: (n, cb) => { let sum = 0; for (let i = 0; i < n; i++) sum += i; cb(sum) },
       bwd: (n, cb) => { let sum = 0; for (let i = n; i--> 0; ) sum += i; cb(sum) },
     }).then(cmpData => {
-      console.log('got some data:', cmpData);
+      // console.log('got some data:', cmpData);
       expect( Object.keys(cmpData).sort() ).to.deep.equal(['bwd', 'fwd']);
-      expect( Object.values(cmpData).map( x => x.length) ).to.deep.equal([4,4]);
+      // expect( Object.values(cmpData).map( x => x.length) ).to.deep.equal([4,4]);
+
+      // console.log('alive');
+      const processed = bench.squashData(cmpData);
+      console.log(processed);
+      expect( Object.keys(processed).sort() ).to.deep.equal( ['bwd', 'fwd', 'n']);
+
+      expect( processed.fwd.length ).to.equal( processed.n.length );
+      expect( processed.bwd.length ).to.equal( processed.n.length );
+
+      // enforce processed.n to be sorted
+      expect( processed.n ).to.deep.equal( processed.n.map(x=>x).sort((x,y) => x-y) );
 
       // TODO actual tests ^)
       done();
-    })
+    }).catch( err => done(err) );
   })
 })
