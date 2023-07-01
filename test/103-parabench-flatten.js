21f7fd900da2c3aa9b3d6dfed7f483c7e3527ef4
Konstantin S. Uvarin <khedin@gmail.com>
2023-07-01 23:07:40 +0200

    [api] Change flattenData to use stats-logscale with winsorized mean

--- test/103-parabench-flatten.js
+++ test/103-parabench-flatten.js
@@ -6,17 +6,17 @@ const { ParaBench } = require( '../lib/para-bench.js' );
 describe( 'ParaBench.flattenData', () => {
     it('squashes diff measurements into one', done => {
         const input = {
-            foo: [{n: 1, time: 0.1}, {n: 1, time: 0.2}, {n: 1, time: 0.3}, {n: 1, time: 0.4}, {n: 1, time: 0.5}, ]
+            foo: [{n: 1, time: 1}, {n: 1, time: 2}, {n: 1, time: 3}, {n: 1, time: 4}, {n: 1, time: 5}, ]
         };
-        expect(ParaBench.flattenData(input)).to.deep.equal({
-            n: [1],
-            times: {
-                foo: [0.1]
-            },
-            ops: {
-                foo: [10]
-            }
-        });
+        const result = ParaBench.flattenData(input);
+
+        console.log(result);
+
+        expect(result?.n).to.deep.equal([1]);
+        expect(result?.times?.foo?.length).to.equal(result.n.length);
+        expect(result?.times?.foo?.length).to.equal(result.n.length);
+        expect(result.times.foo[0]).to.be.within(2.999, 3.001);
+        expect(result.ops.foo[0]).to.be.within(1/3.001, 1/2.999);
 
         done();
     });
