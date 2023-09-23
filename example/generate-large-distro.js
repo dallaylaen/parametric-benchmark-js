de77a8e5f7c4567570665806f2d55d1093b7d955
Konstantin S. Uvarin <khedin@gmail.com>
2023-09-23 19:05:05 +0200

    [test] Add some stupid example

--- example/generate-large-distro.js
+++ example/generate-large-distro.js
@@ -3,11 +3,11 @@
 const { Univariate } = require('stats-logscale');
 const { ParaBench } = require('../lib/para-bench');
 
-const repeat = (n, cb) => n > 0 ? process.nextTick( _ => repeat(n-1, cb)) : cb();
+const repeat = (n, cb) => n > 0 ? setTimeout( _ => repeat(n-1, cb), 0) : cb();
 
 const bench = new ParaBench().addAsync('time0', repeat);
 
-bench.compare({argList: [200], repeat: 1000}).then( data => {
+bench.compare({maxTime: 10, repeat: 300}).then( data => {
     const stat = new Univariate();
     stat.add(...data['time0'].map( x => x.time ) );
     console.log(JSON.stringify(stat));
